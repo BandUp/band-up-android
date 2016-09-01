67f462364c2c9021c98553aa796a9ffef306bb04
rafaelmMaster <motyl93@hotmail.com>
2016-09-01 20:49:42 +0000

    Adding Sign Up view

--- app/src/main/java/com/melodies/bandup/MainActivity.java
+++ app/src/main/java/com/melodies/bandup/MainActivity.java
@@ -4,17 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    public void onClickRegister(View v) {
-        Intent registerIntent = new Intent(MainActivity.this, Register.class);
-        MainActivity.this.startActivity(registerIntent);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSignUp(View v) {
+        final Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        if (v.getId() == R.id.btnSignUp) {
+            Intent signUpIntent = new Intent(MainActivity.this, Register.class);
+            MainActivity.this.startActivity(signUpIntent);
+        }
     }
 }
--- app/src/main/java/com/melodies/bandup/Register.java
+++ app/src/main/java/com/melodies/bandup/Register.java
@@ -1,8 +1,13 @@
 package com.melodies.bandup;
 
-/**
- * Created by Linux on 1.9.2016.
- */
-public class Register {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+public class Register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 }
+

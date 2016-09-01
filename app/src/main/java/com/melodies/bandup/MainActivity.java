1b6213821c8921801e5afe46a820bc45c8809262
Elvar Laxdal <elvarlax@gmail.com>
2016-09-01 18:56:55 +0000

    Android Socket.io Client Skeleton

--- app/src/main/java/com/melodies/bandup/MainActivity.java
+++ app/src/main/java/com/melodies/bandup/MainActivity.java
@@ -4,45 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
-
-import java.net.URISyntaxException;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     public void onClickRegister(View v) {
         Intent registerIntent = new Intent(MainActivity.this, Register.class);
         MainActivity.this.startActivity(registerIntent);
     }
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.toChat:
-                setContentView(R.layout.chat_window);
-        }
-    }
-
-    private Socket mSocket;
-    {
-        try {
-            mSocket = IO.socket("http://192.168.144.131:8080");
-        } catch (URISyntaxException e) {
-        }
-
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSocket.connect();
-        Button mClickButton1 = (Button)findViewById(R.id.toChat);
-        mClickButton1.setOnClickListener(this);
-        System.out.println("Connected?");
-        System.out.println(mSocket.connected());
-
     }
 }

52fcc550162d1d807adcc5498269c5597b3d2521
rafaelmMaster <motyl93@hotmail.com>
2016-09-01 21:04:08 +0000

    onClickSignIn button takes data and does nothing

--- app/src/main/java/com/melodies/bandup/MainActivity.java
+++ app/src/main/java/com/melodies/bandup/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -13,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // when Sign In is Clicked grab data and ...
+    public void onClickSignIn(View v) {
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        final String username = etUsername.getText().toString();
+        final String password = etPassword.getText().toString();
+
+        if (v.getId() == R.id.btnSignIn) {
+            // username
+            // password
+            // ...do stuff
+        }
+    }
+
+    // when Sign Up is Clicked go to Registration View
     public void onClickSignUp(View v) {
         final Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
 

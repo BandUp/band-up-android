0ad4d79a0c1793192dc9327ba28accd1b3f31358
bergthor13 <bergthor13@ru.is>
2016-09-01 11:56:55 +0000

    Adding Socket.IO connection

--- app/src/main/java/com/melodies/bandup/MainActivity.java
+++ app/src/main/java/com/melodies/bandup/MainActivity.java
@@ -1,13 +1,42 @@
 package com.melodies.bandup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.toChat:
+                setContentView(R.layout.chat_window);
+        }
+    }
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://192.168.144.131:8080");
+        } catch (URISyntaxException e) {
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSocket.connect();
+        Button mClickButton1 = (Button)findViewById(R.id.toChat);
+        mClickButton1.setOnClickListener(this);
+        System.out.println("Connected?");
+        System.out.println(mSocket.connected());
+
     }
 }

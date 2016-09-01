584a770c29545432ddd9920a9df534f5815b1ded
rafaelmMaster <motyl93@hotmail.com>
2016-09-01 20:51:28 +0000

    binding view to variables & converting to string

--- app/src/main/java/com/melodies/bandup/Register.java
+++ app/src/main/java/com/melodies/bandup/Register.java
@@ -2,6 +2,9 @@ package com.melodies.bandup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
     @Override
@@ -9,5 +12,24 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    public void onClickRegister(View v) {
+        // binding variables
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etPassword2 = (EditText) findViewById(R.id.etPassword2);
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final Button btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        // converting to string
+        final String email = etEmail.getText().toString();
+        final String username = etUsername.getText().toString();
+        final String password = etPassword.getText().toString();
+        final String password2 = etPassword2.getText().toString();
+        final String age = etAge.getText().toString();
+
+    }
+
 }
 

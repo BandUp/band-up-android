a27c1e6bc7a8747d2242cf7b772517810094dfce
rafaelmMaster <motyl93@hotmail.com>
2016-09-12 12:00:15 +0000

    Adding field check before SignIn

--- app/src/main/java/com/melodies/bandup/Login.java
+++ app/src/main/java/com/melodies/bandup/Login.java
@@ -46,38 +46,54 @@ public class Login extends AppCompatActivity {
         final String password = etPassword.getText().toString();
 
         if (v.getId() == R.id.btnSignIn) {
-            // create request for Login
-            JSONObject user = new JSONObject();
+            // Check for empty field in the form
+            if (username.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Please enter your Username.", Toast.LENGTH_SHORT).show();
+            }
+            else if (password.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Please enter your Password.", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                loginUser(username, password);
+            }
+        }
+    }
+
+    // Login user into app
+    private void loginUser(String username, String password) {
+        // create request for Login
+        JSONObject user = new JSONObject();
+        try {
             user.put("username", username);
             user.put("password", password);
-
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.POST,
-                    url,
-                    user,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-
-                            // how to handle respnse correctly??
-                            Toast.makeText(Login.this, "Login Succesful!", Toast.LENGTH_SHORT).show();
-                            Toast.makeText(Login.this, response.toString(), Toast.LENGTH_SHORT).show();
-                            Intent instrumentsIntent = new Intent(Login.this, Instruments.class);
-                            Login.this.startActivity(instrumentsIntent);
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(Login.this, error.toString(), Toast.LENGTH_LONG).show();
-                            System.out.println(error.toString());
-                        }
-                    }
-            );
-
-            // insert request into queue
-            VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                user,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(Login.this, "Login Succesful!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Login.this, response.toString(), Toast.LENGTH_SHORT).show();
+                        Intent instrumentsIntent = new Intent(Login.this, Instruments.class);
+                        Login.this.startActivity(instrumentsIntent);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this, error.toString(), Toast.LENGTH_LONG).show();
+                        System.out.println(error.toString());
+                    }
+                }
+        );
+
+        // insert request into queue
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 
     // when Sign Up is Clicked go to Registration View

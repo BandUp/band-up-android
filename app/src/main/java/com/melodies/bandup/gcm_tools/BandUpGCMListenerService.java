955b106c1caada10d05ef6e94089778bc973e97f
Dagur Arinbjörn Daníelsson <dagur13@ru,is>
2016-10-20 19:42:53 +0000

    notifications V1.0

--- app/src/main/java/com/melodies/bandup/gcm_tools/BandUpGCMListenerService.java
+++ app/src/main/java/com/melodies/bandup/gcm_tools/BandUpGCMListenerService.java
@@ -1,11 +1,19 @@
 package com.melodies.bandup.gcm_tools;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
 import com.melodies.bandup.MainScreenActivity.MainScreenActivity;
+import com.melodies.bandup.R;
 
 /**
  * Created by Notandi on 20.10.2016.
@@ -35,5 +43,21 @@ public class BandUpGCMListenerService extends GcmListenerService {
     private void sendNotification(String message){
         Intent intent = new Intent(this, MainScreenActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /*Request code*/, intent,
+                                                                PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.cast_ic_notification_small_icon)
+                .setContentTitle("Bad Melodies")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /*ID of notification*/, notificationBuilder.build());
     }
 }

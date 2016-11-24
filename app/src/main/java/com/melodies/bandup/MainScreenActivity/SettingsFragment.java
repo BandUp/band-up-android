bc9729a5e20fca64e432174ef2487344fb855a0d
rafaelmMaster <motyl93@hotmail.com>
2016-11-24 14:59:07 +0000

    adding to settings

--- app/src/main/java/com/melodies/bandup/MainScreenActivity/SettingsFragment.java
+++ app/src/main/java/com/melodies/bandup/MainScreenActivity/SettingsFragment.java
@@ -27,15 +27,9 @@ import org.json.JSONObject;
 
 import static com.melodies.bandup.MainScreenActivity.ProfileFragment.DEFAULT;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SettingsFragment extends Fragment {
+
     private OnFragmentInteractionListener mListener;
 
     private static int   searchRadius = 0;
@@ -56,18 +50,6 @@ public class SettingsFragment extends Fragment {
     private TextView     txtTermsOfService;
 
 
-    // Required empty public constructor
-    public SettingsFragment() {}
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
@@ -78,8 +60,6 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
     }
 
     @Override
@@ -89,26 +69,19 @@ public class SettingsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
         initializeViews(rootView);
-        initializeActions(rootView);
 
         return rootView;
     }
 
-    private void initializeActions(View rootView) {
-        // Adding ad Banner
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
 
-    }
-
+    // initializing all Views
     private void initializeViews(View rootView) {
-        mAdView           = (AdView)rootView.findViewById(R.id.adView);
-        radiusInitializer(rootView);
-        seekBarAges       = (RangeSeekBar) rootView.findViewById(R.id.seekBarAges);
-        switchMatches     = (Switch)rootView.findViewById(R.id.switchMatches);
-        switchMessages    = (Switch)rootView.findViewById(R.id.switchMessages);
-        switchAlert       = (Switch)rootView.findViewById(R.id.switchAlert);
-        switchUnit        = (Switch)rootView.findViewById(R.id.switchUnit);
+        adInit(rootView);
+        radiusInit(rootView);
+        ageInit(rootView);
+        notificationMatchesInit(rootView);
+        notificationMessagesInit(rootView);
+        notificationAlertInit(rootView);
         txtContact        = (TextView)rootView.findViewById(R.id.txtContact);
         txtHelp           = (TextView)rootView.findViewById(R.id.txtHelp);
         txtSupport        = (TextView)rootView.findViewById(R.id.txtSupport);
@@ -118,19 +91,89 @@ public class SettingsFragment extends Fragment {
         txtTermsOfService = (TextView)rootView.findViewById(R.id.txtTermsOfService);
     }
 
-    // all behaviour of radiusBarUnit
-    private void radiusInitializer(View rootView) {
+    // if switch Inactive Alert is checked send notifications to user, else do not
+    private void notificationAlertInit(View rootView) {
+        switchAlert = (Switch)rootView.findViewById(R.id.switchAlert);
+        if (switchAlert.isChecked()) {
+            //TODO: send notification
+        }
+        else {
+            //TODO: don't send any notifications
+        }
+    }
+
+    // if switch messages is checked send notifications to user about new message, else do not
+    private void notificationMessagesInit(View rootView) {
+        switchMessages = (Switch)rootView.findViewById(R.id.switchMessages);
+        if (switchMessages.isChecked()) {
+            //TODO: send notification to user about new message
+        }
+        else {
+            //TODO: don't send any new message notification to user
+        }
+    }
+
+    // if switch matches is checked send notifications to user about new matches, else do not
+    private void notificationMatchesInit(View rootView) {
+        switchMatches = (Switch)rootView.findViewById(R.id.switchMatches);
+        if (switchMatches.isChecked()) {
+            //TODO: send notification to user about new matches
+        }
+        else {
+            //TODO: don't send any new matces notification to user
+        }
+    }
+
+    // Adding ad Banner
+    private void adInit(View rootView) {
+        mAdView = (AdView)rootView.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    // initializing min and max age, and listen for user criteria
+    private void ageInit(View rootView) {
+        seekBarAges = (RangeSeekBar) rootView.findViewById(R.id.seekBarAges);
+        RangeSeekBar<Integer> seekBar = new RangeSeekBar<Integer>(getActivity());
+        seekBarAges.setRangeValues(13, 99);     // min and max age
+
+        seekBarAges.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minAge, Integer maxAge) {
+                // TODO: store searchrangeAge in DB||SharedPreferences which is better?
+                saveUserCredentials("minAge", minAge);
+                saveUserCredentials("maxAge", maxAge);
+            }
+        });
+    }
+
+    // Saving user credentials on User Phone
+    public void saveUserCredentials(String valueName, Integer value) {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("SettingsFile", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(valueName, value);
+        editor.apply();
+        Toast.makeText(getActivity(), "Saved: "+value , Toast.LENGTH_SHORT).show();
+    }
+
+    // Loading user credentials
+    public Integer loadUserCredentials(String valueName) {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("SettingsFile", Context.MODE_PRIVATE);
+        return Integer.valueOf(sharedPreferences.getString(valueName, DEFAULT));
+    }
+
+    // all behaviour of radiusBarUnit is placed in this function
+    private void radiusInit(View rootView) {
+        switchUnit = (Switch)rootView.findViewById(R.id.switchUnit);
         txtRadius = (TextView)rootView.findViewById(R.id.txtRadius);
         seekBarRadius = (SeekBar)rootView.findViewById(R.id.seekBarRadius);
         seekBarRadius.setProgress(25);      // Default progress value
 
-
         seekBarRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // TODO: Turn on GPS, if it's off, and get user newest location
 
-                // if unit switch is mi, put km in text view else km
+                // if unit switch is on, put mi in text view else km
                 if (switchUnit.isChecked()) {
                     seekBarRadius.setMax(186);      // Maximum value of search range in Mi
                     String radius = String.format("%s %s %s", "Radius ", Integer.toString(progress), " Mi");
@@ -152,7 +195,7 @@ public class SettingsFragment extends Fragment {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 // store searchRadius into server in one consistent unit / (let's say km)
                 try {
-                    updateUser(getUserId(), searchRadius);
+                    updateUser(getUserId(), "searchradius", searchRadius);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -167,18 +210,18 @@ public class SettingsFragment extends Fragment {
         return (!id.equals(DEFAULT)) ? id : "User ID Not Found";
     }
 
-    // Send updated user data to server
-    public void updateUser(String id, final int data) {
+    // Send updated user data to server, takes current User ID, mongo Schema Attr, and data to be updated
+    public void updateUser(String id, String schemaAttr, final int data) {
         JSONObject userUpdated = new JSONObject();
         try {
             userUpdated.put("_id", id);
-            userUpdated.put("searchradius", data);
+            userUpdated.put(schemaAttr, data);
 
             DatabaseSingleton.getInstance(getActivity()).getBandUpDatabase().updateUser(userUpdated, new BandUpResponseListener() {
                 @Override
                 public void onBandUpResponse(Object response) {
                     // succesful response
-                    Toast.makeText(getActivity(), "Range updated! REMOVE THIS TOAST", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Data updated! REMOVE THIS TOAST", Toast.LENGTH_SHORT).show();
                 }
             }, new BandUpErrorListener() {
                 @Override
@@ -195,10 +238,10 @@ public class SettingsFragment extends Fragment {
     // Converts miles into whole kilometers for consistent search range storage
     private int milesToKilometers(int miles) {
         double kilometers = miles * 1.609344;
-        int range = (int) Math.ceil(kilometers);
-        return range;
+        return (int) Math.ceil(kilometers);
     }
 
+    // is called whenever we attach fragment to activity
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -216,7 +259,7 @@ public class SettingsFragment extends Fragment {
         mListener = null;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -234,7 +277,6 @@ public class SettingsFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }

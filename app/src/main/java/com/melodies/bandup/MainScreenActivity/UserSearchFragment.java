6b6f42de0f3a0dee6936603ceb883d18ab0ffb9f
Dagur Arinbjörn Daníelsson <dagur13@ru,is>
2016-11-24 17:29:30 +0000

    search fragment, genres selection

--- app/src/main/java/com/melodies/bandup/MainScreenActivity/UserSearchFragment.java
+++ app/src/main/java/com/melodies/bandup/MainScreenActivity/UserSearchFragment.java
@@ -87,7 +87,7 @@ public class UserSearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View rootView = inflater.inflate(R.layout.fragment_user_search, container, false);
         findViews(rootView);
         return rootView;

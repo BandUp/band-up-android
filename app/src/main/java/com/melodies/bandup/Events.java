d89cc0efbf910ce61c91d9d9483b41237248246f
Elvar Laxdal <elvarlax@gmail.com>
2017-02-09 17:25:38 +0000

    Adding eventList

---
+++ app/src/main/java/com/melodies/bandup/Events.java
@@ -0,0 +1,13 @@
+package com.melodies.bandup;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Events extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+}

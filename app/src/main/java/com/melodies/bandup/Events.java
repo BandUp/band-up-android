d95440da78a95a9514e67aa7abb9616f62a4992a
Elvar Laxdal <elvarlax@gmail.com>
2017-02-09 17:26:56 +0000

    And forgot to add this

--- app/src/main/java/com/melodies/bandup/Events.java
+++ app/src/main/java/com/melodies/bandup/Events.java
@@ -1,13 +1,32 @@
 package com.melodies.bandup;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class Events extends Activity {
+import java.util.ArrayList;
+
+public class Events extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+        ListView eventList = (ListView) findViewById(R.id.eventList);
+
+        ArrayList<String> events = new ArrayList<String>();
+
+        events.add("Elvar");
+        events.add("Dagur");
+        events.add("Bergþór");
+        events.add("Rafá");
+
+        /* TODO: Add events to ArrayList */
+
+        eventList.setAdapter(new ArrayAdapter<String>(this, // context
+                                                      android.R.layout.simple_list_item_1, // layout
+                                                      events)); // data
     }
 }
